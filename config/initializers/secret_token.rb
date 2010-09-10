5543b110aff074888fdcaff5a7017c8459ef253e
Eric Borgen <ericborgen@gmail.com>
2010-09-09 22:29:40 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChannelShare::Application.config.secret_token = '9913307e08c397ae014275b7c67a550aa0b3945d68be8354207355fca30121a5d3fb84c3963bff68ec046c4293e73b15c1d07c073c38a6ea53bd8347a06652dd'
